1227ecd12c16b7adec261e8a02d168dacee137d3
Kadin Vo <kadinvo@gmail.com>
2023-11-22 23:13:28 +1300

    Attempt to debug why interaction is not working

--- commands/testcommand.js
+++ commands/testcommand.js
@@ -1,11 +1,9 @@
 import { SlashCommandBuilder } from 'discord.js'
 
-// Command Builder export
 export const data = new SlashCommandBuilder()
   .setName('test')
   .setDescription('This is a test description')
 
-// Execute function export
 export async function execute(interaction) {
   await interaction.reply('testing123')
 }
--- index.js
+++ index.js
@@ -8,6 +8,22 @@ function readyDiscord() {
   console.log('test' + client.user.tag)
 }
 
+async function handleCommand(interaction) {
+  try {
+    if (!interaction.isCommand()) return
+    if (interaction.commandName === 'test') {
+      await test.execute(interaction)
+    }
+  } catch (error) {
+    console.error(error)
+    await interaction.reply('An error occurred while processing the command.')
+  }
+}
+
 client.once(Events.ClientReady, readyDiscord)
 
 client.login(process.env.DISCORD_TOKEN)
+
+client.on(Events.InteractionCreate, handleCommand)
+
+client.on(Events.Debug, console.log)
