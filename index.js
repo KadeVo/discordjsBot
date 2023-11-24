72a585419809e4c89a140ce014ad7eb2e602daf7
Kadin Vo <kadinvo@gmail.com>
2023-11-24 21:15:50 +1300

    Added Intent guild messages to test

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ import { Client, Events, GatewayIntentBits } from 'discord.js'
 import { config } from 'dotenv'
 import * as test from './commands/testcommand.js'
 config()
-const client = new Client({ intents: [GatewayIntentBits.Guilds] })
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+})
 
 function readyDiscord() {
   console.log('test' + client.user.tag)
