c02e94f2af9d83137aa4dfff2d56b33e23254851
Kadin Vo <kadinvo@gmail.com>
2024-02-09 15:12:04 +1300

    Changed help to array to display multiple commands better

--- commands/help.js
+++ commands/help.js
@@ -5,7 +5,28 @@ export const data = new SlashCommandBuilder()
   .setDescription('Print our commands')
 
 export async function execute(interaction) {
-  await interaction.reply(
-    `/diceroll (enter your max number), /coinflip, /question (enter your question) (enter your options)`
-  )
+  const help = {
+    color: '#0099ff',
+    title: 'Command List',
+    description: 'Here is a list of available commands:',
+    fields: [
+      {
+        name: '/diceroll [max number]',
+        value: 'Roll a dice with the specified maximum number',
+      },
+      {
+        name: '/coinflip',
+        value: 'Flip a coin to get heads or tails',
+      },
+      {
+        name: '/question [question] [options]',
+        value: 'Set up a question to ask everyone',
+      },
+    ],
+    footer: {
+      text: 'For more information or assistance, contact the bot owner.',
+    },
+  }
+
+  await interaction.reply({ embeds: [help] })
 }
